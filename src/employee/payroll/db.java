442e4fe7a08d1da13dfbeb7a58dc39a186befadf
Ye11ow-Flash <jaineel.ns@somaiya.edu>
2017-11-08 13:36:20 +0530

    initial commit

---
+++ src/employee/payroll/db.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.payroll;
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Jaineel Shah
+ */
+public class db {
+    
+    Connection conn = null;
+    public static Connection java_db(){
+        
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:/Users/Jaineel Shah/Documents/NetBeansProjects/Employee Payroll/mydatabase.sqlite");
+            return conn;
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+        }
+        
+    }
+    
+}
